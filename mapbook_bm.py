cfad0b15a74c5b81056257dfd8dd26d2c135211e
Noufa <cknoufa94@gmail.com>
2018-08-29 16:53:12 +0530

    added comments

--- mapbook_bm.py
+++ mapbook_bm.py
@@ -1,7 +1,12 @@
+#python snippet to create a pdfmapbook using bookmarks as the parameter to loop through.
+#For end of the term Assignment of Application Development IP
+#Submitted by Noufa Cheerakkollil Konath and Venkatesh Nitchenakolla
+
 import arcpy
 
 aprx= arcpy.mp.ArcGISProject(r"G:\AppDevIP\India\India2.aprx")
 
+#list method for listing map, layers and layouts in the project 
 Maps = aprx.listMaps()[0]
 lyrInBoun = Maps.listLayers()[0]
 lyrInStates= Maps.listLayers()[1]
@@ -9,27 +14,30 @@ lyrMaxTemp= Maps.listLayers()[2]
 lyt = aprx.listLayouts()[0]
 print(lyt.name)
 
- 
-#Create an empty PDF document
+
+#define various elements to be used in the snippet later on
 mapFrame = lyt.listElements("MAPFRAME_ELEMENT")[0]
 Title_elm= lyt.listElements("TEXT_ELEMENT")
 Legend=lyt.listElements("LEGEND_ELEMENT")
+
+#Create an empty pdf document into which the different .pdf files will be added.
 outPath = "G:\\AppDevIP\\Mapbooks" 
 pdfDoc = arcpy.mp.PDFDocumentCreate(outPath  + "StateTemp.pdf")
 
-
+#append the title page to the mapbook
 Layout1 = aprx.listLayouts()[1]
 Layout1.exportToPDF(outPath + "first page" + ".PDF", 100)
 pdfDoc.appendPages (outPath + "first page" + ".PDF")
 
-#change name of the title of the map
 
+#change name of the title of the maps 
 for txt in lyt.listElements("TEXT_ELEMENT"):
     if txt.text == "TITLE":
          Title = txt
         
-
+#change name of the title of the maps according to the name of the states, which are refered from bookmarks that are already created
 bookmarks= mapFrame.map.listBookmarks()
+#loop through the list of bookmarks and zoom into its extent, and then change the name of the map according to the name of bookmark.
 for bm in bookmarks:
     mapFrame.zoomToBookmark(bm)
     for layer in Maps.listLayers("INDIAStates"):
@@ -39,9 +47,6 @@ for bm in bookmarks:
         lyt.exportToPDF(outPath + name +".PDF", 100)
         pdfDoc.appendPages (outPath + name +".PDF")
         
-
-    
-  
 
 pdfDoc.saveAndClose ()
 print ("PDF Mapbook created")
--- mapbook_cur.py
+++ mapbook_cur.py
@@ -1,34 +1,41 @@
+#python snippet to create a pdfmapbook using da.cursor as the parameter to loop through.
+#For end of the term Assignment of Application Development IP
+#Submitted by Noufa Cheerakkollil Konath and Venkatesh Nitchenakolla 
 
 import arcpy
 
 aprx= arcpy.mp.ArcGISProject(r"G:\AppDevIP\India\India2.aprx")
 
+#list method for listing map, layers and layouts in the project 
 Maps = aprx.listMaps()[0]
 lyrInBoun = Maps.listLayers()[0]
 lyrInStates= Maps.listLayers()[1]
 lyrMaxTemp= Maps.listLayers()[2]
 lyt = aprx.listLayouts()[0]
 
-
  
-#Create an empty PDF document
+#define various elements to be used in the snippet later on
 mapFrame = lyt.listElements("MAPFRAME_ELEMENT")[0]
 Title_elm= lyt.listElements("TEXT_ELEMENT")
 Legend=lyt.listElements("LEGEND_ELEMENT","Legend")
+
+#Create an empty pdf document into which the different .pdf files will be added.
 outPath = "G:\\AppDevIP\\Mapbooks" 
 pdfDoc = arcpy.mp.PDFDocumentCreate(outPath  + "StateTemp2.pdf")
 
+#append the title page to the mapbook
 Layout1 = aprx.listLayouts()[1]
 Layout1.exportToPDF(outPath + "first page" + ".PDF", 100)
 pdfDoc.appendPages (outPath + "first page" + ".PDF")
 
-#change name of the title of the map
+#change name of the title of the maps 
 for txt in lyt.listElements("TEXT_ELEMENT"):
     if txt.text == "TITLE":
          Title = txt
         
-#add maps to the pdf document (append)
+#change name of the title of the maps according to the name of the states from the table
 cur = arcpy.da.SearchCursor(lyrInStates.dataSource, ["SHAPE@", "ST_NAME"]) 
+#loop through the table, set extend using camera object, and change the name according to the name of the state (field)
 for item in cur:
     for layer in Maps.listLayers("INDIAStates"):
         mapFrame.camera.setExtent(item[0].extent)
